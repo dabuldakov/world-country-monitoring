5ea523398b31577be0d8a141c8fdeb10c2db2887
Dmitry Buldakov <alena.uymenova@gazprombank.ru>
2026-09-26 18:28:32 +0700

    deploy via ghcr and allow new domain

--- controller/src/main/kotlin/org/wcm/controller/configuration/WebMVCConfiguration.kt
+++ controller/src/main/kotlin/org/wcm/controller/configuration/WebMVCConfiguration.kt
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 open class WebMVCConfiguration : WebMvcConfigurer {
 
-    @Value("\${application.frontend.allowed-origins:http://localhost:3000,http://country-monitoring.com,https://country-monitoring.com,http://www.country-monitoring.com,https://www.country-monitoring.com}")
+    @Value("\${application.frontend.allowed-origins:http://localhost:3000,http://country-monitoring.com,https://country-monitoring.com,http://www.country-monitoring.com,https://www.country-monitoring.com,http://countrymonitoring.ru,https://countrymonitoring.ru,http://www.countrymonitoring.ru,https://www.countrymonitoring.ru}")
     private lateinit var allowedOrigins: String
 
     override fun addCorsMappings(registry: CorsRegistry) {
